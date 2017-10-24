b42e1838d9d0c7f0a8a96acd0d2cf694bca3571c
Tamara Temple <tamouse@gmail.com>
2017-10-24 10:23:40 -0700

    Add authorization header to client.sj

--- ui/src/client.js
+++ ui/src/client.js
@@ -2,14 +2,26 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { ApolloProvider } from 'react-apollo';
 import { ApolloClient } from 'apollo-client';
+import {ApolloLink} from 'apollo-link';
 import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import App from './components/App';
 
 // http://localhost:8080 is put behind a proxy by webpack (cors)
 const uri = '/graphql';
+const httpLink = new HttpLink({ uri });
+const authLink = new ApolloLink((operation, forward) => {
+  const token = localStorage.getItem('AUTH_TOKEN');
+  console.log(token)
+  operation.setContext(()=>({
+    headers: {
+      authorization: token ? `JWT ${token}` : null,
+    }
+  }));
+  return(forward(operation));
+})
 
-const link = new HttpLink({ uri });
+const link = ApolloLink.from([authLink, httpLink])
 
 const cache = new InMemoryCache();
 
